8a55ff248c20e65efa15c66808abdfdf302ac591
Yatin Bhalani <kano24022@gmail.com>
2023-10-27 00:35:39 +0530

    fix - change in server.js file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import RazorPay from "razorpay";
 import app from "./app.js";
 import connectToDB from "./config/db.config.js";
 
+connectToDB();
 import "./config/cloudinary.config.js";
 
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,6 @@ export const razorpay = new RazorPay({
   key_secret: process.env.RAZORPAY_SECRET,
 });
 
-app.listen(PORT, async () => {
-  await connectToDB();
+app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
